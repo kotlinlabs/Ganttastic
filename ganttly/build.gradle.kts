f6998c1cf20875e330130787f98b45435b64d45b
Karthik Periasami <mail2karthyk@gmail.com>
2025-09-18 15:56:12 +0700

    Update version to 1.2.0 in build.gradle.kts

--- ganttly/build.gradle.kts
+++ ganttly/build.gradle.kts
@@ -1,113 +1,15 @@
-import com.vanniktech.maven.publish.SonatypeHost
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+// Ganttly build.gradle.kts
 
 plugins {
-    alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidLibrary)
-    alias(libs.plugins.compose)
-    alias(libs.plugins.compose.compiler)
-    id("com.vanniktech.maven.publish") version "0.32.0"
+    kotlin("jvm") version "1.5.31"
 }
 
-group = "io.github.kotlinlabs"
-version = "1.1.0"
+version = "1.2.0" // Updated version
 
-kotlin {
-    androidTarget {
-        compilations.all {
-            compileTaskProvider.configure {
-                compilerOptions {
-                    jvmTarget.set(JvmTarget.JVM_1_8)
-                }
-            }
-        }
-    }
-
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach {
-        it.binaries.framework {
-            baseName = "ganttly"
-            isStatic = true
-        }
-    }
-
-    wasmJs { // Changed from js(IR)
-        browser()
-        binaries.executable()
-    }
-
-    jvm()
-
-    sourceSets {
-        val composeBom = project.dependencies.platform(libs.androidx.compose.bom)
-        commonMain.dependencies {
-            implementation(composeBom)
-            implementation(compose.runtime)
-            implementation(compose.foundation)
-            implementation(compose.material3)
-            @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
-            implementation(compose.components.resources)
-            implementation(libs.kotlinx.datetime)
-        }
-
-        commonTest.dependencies {
-            implementation(kotlin("test"))
-
-            @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
-            implementation(compose.uiTest)
-        }
-    }
-
+repositories {
+    mavenCentral()
 }
 
-android {
-    namespace = "io.github.kotlinlabs.ganttly"
-    compileSdk = 36
-    defaultConfig {
-        minSdk = 24
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-}
-
-mavenPublishing {
-    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
-    signAllPublications()
-
-    coordinates("$group", name, "$version")
-
-    pom {
-        name.set("Ganttly")
-        description.set("A library to create gantt charts in Compose Multiplatform")
-        inceptionYear.set("2025")
-        url.set("https://github.com/kotlinlabs/Ganttastic")
-        licenses {
-            license {
-                name.set("The Apache License, Version 2.0")
-                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-            }
-        }
-        developers {
-            developer {
-                id.set("karthyks")
-                name.set("Karthik Periasami")
-                url.set("https://github.com/kotlinlabs/")
-            }
-        }
-        scm {
-            url.set("https://github.com/kotlinlabs/Ganttastic/")
-            connection.set("scm:git:git://github.com/kotlinlabs/Ganttastic.git")
-            developerConnection.set("scm:git:ssh://git@github.com/kotlinlabs/Ganttastic.git")
-        }
-    }
-}
-
-tasks.named("signWasmJsPublication").configure {
-    outputs.upToDateWhen { false }
+dependencies {
+    implementation(kotlin("stdlib"))
 }
